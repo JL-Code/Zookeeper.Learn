ec4718a9317d46d8ef84f6c576c83877bcd29273
jiangy <jiangliu0316@outlook.com>
2017-10-16 15:50:47 +0800

    init

---
+++ Zookeeper.Core/ConnectionedWatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using org.apache.zookeeper;
+
+namespace Zookeeper.Core
+{
+    public class ConnectionedWatcher : Watcher
+    {
+
+        private readonly Action _connectioned;
+        private readonly Action _disconnected;
+
+        public ConnectionedWatcher(Action connectioned, Action disconnected)
+        {
+            _connectioned = connectioned;
+            _disconnected = disconnected;
+        }
+
+        /// <summary>
+        /// Process the specified event.
+        /// </summary>
+        /// <returns>The process.</returns>
+        /// <param name="event">Event.</param>
+        public override Task process(WatchedEvent @event)
+        {
+            if (@event.getState() == Event.KeeperState.SyncConnected)
+            {
+                _connectioned();
+            }
+            else
+            {
+                _disconnected();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Zookeeper.Core/ZookeeperManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using org.apache.zookeeper;
+
+namespace Zookeeper.Core
+{
+    public class ZookeeperManager
+    {
+        private string _host;
+        private int _timeout;
+        protected ZooKeeper _zk;
+        private ManualResetEvent _connectWait = new ManualResetEvent(false);
+
+        public ZookeeperManager(string host, int timeout = 5000)
+        {
+            _host = host;
+            _timeout = timeout;
+            ConnectZookeeper().Wait();
+        }
+
+        /// <summary>
+        /// Connects the zookeeper.
+        /// </summary>
+        /// <returns>The zookeeper.</returns>
+        private async Task ConnectZookeeper()
+        {
+            if (_zk != null)
+            {
+                await _zk.closeAsync();
+            }
+            _zk = new ZooKeeper(_host, _timeout, new ConnectionedWatcher(() =>
+            {
+                _connectWait.Set();
+            }, async () =>
+            {
+                _connectWait.Reset();
+                await ConnectZookeeper();
+            }));
+        }
+    }
+}
